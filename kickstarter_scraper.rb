59e20484988b05a084c4cab782844da0a9c045f1
Raveen K Sharma <raveen08@gmail.com>
2020-05-25 22:44:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,3 +10,14 @@ require 'pry'
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
  
+def create_project_hash
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+ 
+  # return the projects hash
+  projects
+end
