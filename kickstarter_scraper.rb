4faf54ac51f4644c66d7a9a4c29fb9d5873aa70f
Raveen K Sharma <raveen08@gmail.com>
2020-05-25 22:22:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,4 +2,11 @@
 
 def create_project_hash
   # write your code here
+  # This just opens a file and reads it into a variable
+html = File.read('fixtures/kickstarter.html')
+ 
+kickstarter = Nokogiri::HTML(html)
+  
+  
+  
 end
